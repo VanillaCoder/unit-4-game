57cb22e4a21931899f0965c0f5818c3f4bc0e0eb
LiamH <liamhearts@gmail.com>
2019-04-04 10:46:45 -0700

    style and variables set

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,36 @@
 // THINGS TO TRACK
-//value of the gems
-//players current points
-//points required to win
-//wins
-//losses
+
 
 // THINGS TODO
-//TODO: randomly assign gems a value between 1-12
 //TODO: randomize gem value every game
 //TODO: set a winning score between 19-120
 //TODO: reset game on win or loss
 //TODO: increase win/loss accordingly
 //TODO: when a gem is clicked, increase current score by the gems hidden value
+
+
+var gem1 = 0;
+var gem2 = 0;
+var gem3 = 0;
+var gem4 = 0;
+var currentPoints = 0;
+var winScore = 0;
+var wins = 0;
+var wins = 0;
+
+
+function gemValue(){
+    gem1 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+    gem2 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+    gem3 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+    gem4 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
+    currentPoints = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+
+}
+
+gemValue();
+console.log(gem1);
+console.log(gem2);
+console.log(gem3);
+console.log(gem4);
+console.log(currentPoints);
