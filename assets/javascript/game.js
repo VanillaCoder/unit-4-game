c7a4264e366ce9e3b742837b12b8ef363e24ee83
LiamH <liamhearts@gmail.com>
2019-04-03 18:07:06 -0700

    commented into my todo lists

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+// THINGS TO TRACK
+//value of the gems
+//players current points
+//points required to win
+//wins
+//losses
+
+// THINGS TODO
+//TODO: randomly assign gems a value between 1-12
+//TODO: randomize gem value every game
+//TODO: set a winning score between 19-120
+//TODO: reset game on win or loss
+//TODO: increase win/loss accordingly
+//TODO: when a gem is clicked, increase current score by the gems hidden value
