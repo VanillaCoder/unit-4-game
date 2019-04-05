2330997903b535024487a7c6a0e9e293a8d09264
LiamH <liamhearts@gmail.com>
2019-04-04 19:30:35 -0700

    finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,24 +13,64 @@ var gem1 = 0;
 var gem2 = 0;
 var gem3 = 0;
 var gem4 = 0;
-var currentPoints = 0;
+var winningPoints = 0;
+var currentScore = 0;
 var winScore = 0;
 var wins = 0;
-var wins = 0;
+var loss = 0;
 
 
-function gemValue(){
+function gemValue() {
     gem1 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
     gem2 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
     gem3 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
     gem4 = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
-    currentPoints = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+    winningPoints = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+    currentScore = 0;
+    $("#winningScore").text(winningPoints);
+    $("#currentScore").text(currentScore);
+    $("#win").text(wins);
+    $("#loss").text(loss);
 
 }
 
 gemValue();
-console.log(gem1);
-console.log(gem2);
-console.log(gem3);
-console.log(gem4);
-console.log(currentPoints);
+
+
+
+
+
+
+$("#gem1").on("click", function () {
+    currentScore = gem1 + currentScore;
+    $("#currentScore").text(currentScore);
+
+})
+
+$("#gem2").on("click", function () {
+    currentScore = gem2 + currentScore;
+    $("#currentScore").text(currentScore);
+})
+
+$("#gem3").on("click", function () {
+    currentScore = gem3 + currentScore;
+    $("#currentScore").text(currentScore);
+})
+
+$("#gem4").on("click", function () {
+    currentScore = gem4 + currentScore;
+    $("#currentScore").text(currentScore);
+})
+
+$(".gemImg").on("click", function () {
+    if (currentScore > winningPoints) {
+        loss++;
+        console.log(loss);
+        gemValue();
+    }
+    else if (currentScore === winningPoints) {
+        wins++;
+        gemValue();
+    }
+
+})
